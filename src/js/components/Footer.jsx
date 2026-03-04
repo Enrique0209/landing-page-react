f29fad9ec7fb6d5c7506bcc64d4cbdda7e4180b6
Jessica Pinta <enrique.glez.jai@gmail.com>
2026-03-04 13:00:54 +0100

    Versión 1

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer (){
+    return (<div class="card">
+  <div class="card-header">
+    Copyright Your Website 20
+  </div>
+ 
+</div>)
+}
+
+export default Footer;
